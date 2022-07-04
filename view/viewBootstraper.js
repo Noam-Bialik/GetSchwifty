2de8fdb3d339ac06ed80504c2357fd687977e5b8
Noam-Bialik <n.o.bialik@gmail.com>
2022-07-04 13:19:28 +0300

    pass document to keiesInput

--- view/viewBootstraper.js
+++ view/viewBootstraper.js
@@ -4,7 +4,7 @@ console.log("viewBootstraper called");
 
 class ViewBootstraper{
     constructor(){
-        this.keiesInput = new KeiesInput();
+        this.keiesInput = new KeiesInput(document);
         this.table = new Table();
         this.view = new View(this.table, this.keiesInput);
         return this.view;
