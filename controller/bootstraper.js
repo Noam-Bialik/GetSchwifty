bd7239bec1b4853e46d49874b8004b91a8f8a747
Noam-Bialik <n.o.bialik@gmail.com>
2022-07-04 11:49:17 +0300

    basic view-controller flow works

--- controller/bootstraper.js
+++ controller/bootstraper.js
@@ -1,2 +1,12 @@
 "use strict"
 console.log("bootstreper.js called");
+
+class Bootstreper{
+    constructor(){
+        this.view = new ViewBootstraper();
+        this.model = null;
+        this.controller = new Controller(this.view, this.model);
+    }
+}
+
+let bootstreper = new Bootstreper();
--- controller/controller.js
+++ controller/controller.js
@@ -1,2 +1,11 @@
 "use strict"
 console.log("controller.js called");
+
+class Controller{
+    constructor(view, model){
+        this.view = view;
+        this.model = model;
+        let a = [[1,2,3],[4,5,6],[7,8,9]]
+        this.view.showTable(a);
+    }
+}
