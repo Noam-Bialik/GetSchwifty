c60a4dfa26a3fe302e7a24fae5eee37e88706c74
Noam-Bialik <n.o.bialik@gmail.com>
2022-07-04 13:23:03 +0300

    created handlers for the listners

--- controller/controller.js
+++ controller/controller.js
@@ -5,7 +5,29 @@ class Controller{
     constructor(view, model){
         this.view = view;
         this.model = model;
+        this.starter();
+        }
+
+    starter(){
         let a = [[1,2,3],[4,5,6],[7,8,9]]
         this.view.showTable(a);
+        this.view.setOnArrowRight(this.swipeRight);
+        this.view.setOnArrowLeft(this.swipeLeft);
+        this.view.setOnArrowUp(this.swipeUp);
+        this.view.setOnArrowDown(this.swipeDown);
     }
+
+    swipeLeft(){
+        console.log("swipe left");
+    }
+    swipeRight(){
+        console.log("swipe right");
+    }
+    swipeUp(){
+        console.log("swipe up");
+    }
+    swipeDown(){
+        console.log("swipe down");
+    }
+    
 }
