1997712a4ce9dd71730f26dcbb9e083aa21b3a53
Noam-Bialik <n.o.bialik@gmail.com>
2022-07-04 18:18:04 +0300

    added elements that will create from view.js

--- view/view.js
+++ view/view.js
@@ -6,31 +6,63 @@ class View{
     constructor(table, keiesInput){
         this.table = table;
         this.keiesInput = keiesInput;
-        this.root = this.getElement("#root");
-     
+        this.tableContainer = this.getElement("#table_container");
+       
+        this.root = this.getElement('#root');
+        this.form = this.createElement('form');
+        this.tableSize = this.createElement('input');
+        this.tableSize.setAttribute('type', 'number');
+        this.submitButton = this.createElement('button');
+        this.submitButton.textContent = 'start';
+        this.form.append(this.tableSize,  this.submitButton);
+        this.root.append(this.form);
     }
 
     showTable(pieces){
-        this.root.append(this.table.getTableElement(pieces));
+        this.tableContainer.innerHTML = '';
+        this.tableContainer.append(this.table.getTableElement(pieces));
     }
 
     getElement(selector) {
         const element = document.querySelector(selector);
         return element;
-      }
+    }
     
     setOnArrowRight(handler){
         this.keiesInput.setOnKeyDown(39,handler);
     }
+
     setOnArrowLeft(handler){
         this.keiesInput.setOnKeyDown(37,handler);
     }
+
     setOnArrowUp(handler){
         this.keiesInput.setOnKeyDown(38,handler);
     }
+
     setOnArrowDown(handler){
         this.keiesInput.setOnKeyDown(40,handler);
     }
+
+    createElement(tag, className) {
+        const element = document.createElement(tag);
+        if (className){
+             element.classList.add(className);
+            }
+        return element
+    }
+    
+    bindButton(handler){
+        this.form.addEventListener('submit', event =>{
+            this.submitButton.textContent = 'restart';
+            console.log("bottun clicked");
+            event.preventDefault();
+            handler(this.tableSize.value);
+      });
+    }
+    
+     
+    
 
 
 }
